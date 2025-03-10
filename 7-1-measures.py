8dc6382bbd0d3de15ee567d56467c45f009c1988
Чухланцева Анна <chukhlantseva.aa@phystech.edu>
2025-03-10 12:40:58 +0300

    for my sun

--- 7-1-measures.py
+++ 7-1-measures.py
@@ -12,7 +12,7 @@ GPIO.setup(leds, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(troyka, GPIO.OUT)
 GPIO.setup(comp, GPIO.IN)
 
-def adc():
+def adc_1():
     for value in range(256):
         GPIO.output(dac, dec2bin(value))
         time.sleep(0.01)
@@ -21,6 +21,25 @@ def adc():
             time.sleep(0.001)
             return value/255*3.3
     return 3.3
+
+def bin2dec(num):
+    string = '0b'
+    for i in num:
+        string += str(i)
+    return int(string, base=2)
+
+def adc():
+    value = [0]*8
+    for i in range(8):
+        value[i] = 1
+        GPIO.output(dac, value)
+        time.sleep(0.01)
+        comp_value = GPIO.input(comp)
+        if comp_value == 1:
+            value[i] = 0
+    GPIO.output(dac, value)
+    time.sleep(0.01)
+    return bin2dec(value)/255*3.3
 
 def dec2bin(num):
     bi = bin(int(num))[2:].zfill(8)
@@ -31,13 +50,14 @@ try:
     measures = []
     time_start = time.time()
     GPIO.output(troyka, GPIO.HIGH)
-    while adc() <= 0.8*3.3:
+    while adc() <= 0.7*3.3:
         measures.append(adc())
         GPIO.output(leds, dec2bin(adc()))
+        print
     print('разрядка')
     time.sleep(0.01)
     GPIO.output(troyka, GPIO.LOW)
-    while adc() >= 0.2*3.3:
+    while adc() >= 0.4*3.3:
         measures.append(adc())
         GPIO.output(leds, dec2bin(adc()))
     time_finish = time.time()
@@ -53,3 +73,9 @@ try:
 finally:
     GPIO.output(leds, 0)
     GPIO.cleanup()
+    T = final_time/len(measures)
+    n = 1/T
+    qs = 3.3/256
+    with open('settings.txt', 'w') as out:
+        out.write('n = '+str(n)+' qs = '+str(qs))
+    print(f'T = {T}, n = {n}, qs = {qs}')
